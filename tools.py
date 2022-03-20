a08bf0d89a00758d74f46ead969a343809abc93a
ParthGupte <75539624+ParthGupte@users.noreply.github.com>
2022-03-20 12:12:14 +0530

    corrections to tools

--- tools.py
+++ tools.py
@@ -1,6 +1,6 @@
 alphalist = [] #add alphabets in decreasing order of frequency as they appear in the eng language
 def replace(ciphertxt,guesses): #guesses is a dict of key=cipher value value=plain value
-    ciphlist = list(ciphertext)
+    ciphlist = list(ciphertxt)
     L = []
     for i in ciphlist:
         if i in guesses.keys():
