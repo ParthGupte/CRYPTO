7f08ac642a864e2a811976242007404f3114fe10
ParthGupte <75539624+ParthGupte@users.noreply.github.com>
2022-03-20 20:55:09 +0530

    entrytion decrytion done
    output is now ascii

--- encryt_decrypt.py
+++ encryt_decrypt.py
@@ -10,6 +10,19 @@ def vec_encrypt(p,k):
     else:
         print("Enter valid ModMatix objects")
 
+def vectoascii(vec):
+    if vec.isvector():
+        txt = ''
+        for row in vec.array:
+            i = row[0]
+            if i<32:
+                txt += chr(126-i)
+            else:
+                txt += chr(i)
+        return txt
+    else:
+        print("Input should be vector")
+
 def encrypt(p,k):
     base = 127
     n = len(p)
@@ -19,16 +32,16 @@ def encrypt(p,k):
             p_list.append([ord(i)])
         p_list.extend([[ord('~')]]*(400-n))
         p_vec = ModMatix(np.array(p_list),base)
-        return vec_encrypt(p_vec,k)
+        return vectoascii(vec_encrypt(p_vec,k))
     if n>400:
         t = empty(base)
         while len(p)>0:
             t.vec_concat(encrypt(p[:400],k))
             p = p[400:]
-        return t
+        return vectoascii(t)
     
 def decrypt(t,k):
-    encrypt(t,k.inv())
+    return encrypt(t,k.inv()).rstrip('~')
 
 ##### test
 
